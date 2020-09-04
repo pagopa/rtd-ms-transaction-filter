77dc490ffca6c813e0b3fe768f31501cb3805aed
Alessio Cialini <alessio.cialini.sia@gmail.com>
2020-09-04 15:28:12 +0200

    [rtd-ms-transaction-filter/develop] Introduced TransactionReaderStepListenerTest

---
+++ api/batch/src/test/java/it/gov/pagopa/rtd/transaction_filter/batch/step/listener/TransactionReaderStepListenerTest.java
@@ -0,0 +1,29 @@
+package it.gov.pagopa.rtd.transaction_filter.batch.step.listener;
+
+import lombok.SneakyThrows;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
+
+import java.time.format.DateTimeFormatter;
+
+public class TransactionReaderStepListenerTest {
+
+    @SneakyThrows
+    @Test
+    public void afterStepWithSkips() {
+
+        StepExecution stepExecution = new StepExecution("test-step", new JobExecution(1L));
+        stepExecution.setStatus(BatchStatus.COMPLETED);
+        stepExecution.setProcessSkipCount(1);
+
+        TransactionReaderStepListener transactionReaderStepListener = new TransactionReaderStepListener();
+        ExitStatus status = transactionReaderStepListener.afterStep(stepExecution);
+        Assert.assertEquals(new ExitStatus("COMPLETED WITH SKIPS"), status);
+
+    }
+
+}
