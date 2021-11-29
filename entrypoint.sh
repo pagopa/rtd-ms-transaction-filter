680da90c879787c30da0fa6eb8e1910e06cd87d4
Alberto Re <alberto.re@pagopa.it>
2021-11-29 15:29:17 +0100

    [CEN-1068] avoid mounting secrets on /tmp

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
-JKS_ENCODED="/tmp/certs.jks"
 APP_ROOT="/app"
 APP_BIN="$APP_ROOT/app.jar"
 APP_CONFIG="$APP_ROOT/config.yml"
 APP_WORKDIR="/app_workdir"
 APP_TMPDIR="$APP_WORKDIR/tmp"
+JKS_ENCODED="$APP_ROOT/certs.jks.base64"
 JKS_DECODED="$APP_ROOT/certs.jks"
 
 # To remove in favor of configmap values
-export HPAN_SERVICE_KEY_STORE_FILE="$APP_ROOT/certs.jks"
-export HPAN_SERVICE_TRUST_STORE_FILE="$APP_ROOT/certs.jks"
+export HPAN_SERVICE_KEY_STORE_FILE="$JKS_DECODED"
+export HPAN_SERVICE_TRUST_STORE_FILE="$JKS_DECODED"
 export ACQ_BATCH_TOKEN_INPUT_PATH="$APP_WORKDIR/input"
 export ACQ_BATCH_TRX_INPUT_PATH="$APP_WORKDIR/input"
 export ACQ_BATCH_TRX_LOGS_PATH="$APP_WORKDIR/logs"
 export ACQ_BATCH_OUTPUT_PATH="$APP_WORKDIR/output"
 export ACQ_BATCH_HPAN_INPUT_PATH="$APP_WORKDIR/hpans"
+export ACQ_BATCH_INPUT_CRON="0 */5 * * * *"
 
 mkdir -p $ACQ_BATCH_TOKEN_INPUT_PATH
 mkdir -p $ACQ_BATCH_OUTPUT_PATH
