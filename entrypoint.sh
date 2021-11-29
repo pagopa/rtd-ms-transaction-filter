f1d3f6b8593e1631a23857492fa51cfd072f4a6c
Alberto Re <alberto.re@pagopa.it>
2021-11-29 15:07:44 +0100

    [CEN-1068] set Java IO tmpdir with -D flag on startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ APP_ROOT="/app"
 APP_BIN="$APP_ROOT/app.jar"
 APP_CONFIG="$APP_ROOT/config.yml"
 APP_WORKDIR="/app_workdir"
+APP_TMPDIR="$APP_WORKDIR/tmp"
 JKS_DECODED="$APP_ROOT/certs.jks"
 
 # To remove in favor of configmap values
@@ -20,6 +21,9 @@ mkdir -p $ACQ_BATCH_TOKEN_INPUT_PATH
 mkdir -p $ACQ_BATCH_OUTPUT_PATH
 mkdir -p $ACQ_BATCH_TRX_LOGS_PATH
 mkdir -p $ACQ_BATCH_HPAN_INPUT_PATH
+mkdir -p $APP_TMPDIR
 base64 -d $JKS_ENCODED > $JKS_DECODED
 
-cd $APP_WORKDIR && java -jar $APP_BIN --spring.config.location=$APP_CONFIG
+JAVA_OPTS="-Djava.io.tmpdir=$APP_TMPDIR"
+
+java $JAVA_OPTS -jar $APP_BIN --spring.config.location=$APP_CONFIG
