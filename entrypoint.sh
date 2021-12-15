17546ccee3ddc5489c482ea51c87c49e34853c71
Alberto Re <93929693+alberto-re-pagopa@users.noreply.github.com>
2021-12-15 11:18:34 +0100

    [CEN-1086] Remove export statements from entrypoint.sh (#117)

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,19 +7,6 @@ APP_WORKDIR="/app_workdir"
 JKS_ENCODED="/app_certs_in/certs.jks.base64"
 JKS_DECODED="$APP_ROOT/certs.jks"
 
-# To remove in favor of configmap values
-export HPAN_SERVICE_KEY_STORE_FILE="$JKS_DECODED"
-export HPAN_SERVICE_TRUST_STORE_FILE="$JKS_DECODED"
-export ACQ_BATCH_TOKEN_INPUT_PATH="$APP_WORKDIR/input"
-export ACQ_BATCH_TRX_INPUT_PATH="$APP_WORKDIR/input"
-export ACQ_BATCH_TRX_LOGS_PATH="$APP_WORKDIR/logs"
-export ACQ_BATCH_OUTPUT_PATH="$APP_WORKDIR/output"
-export ACQ_BATCH_HPAN_INPUT_PATH="$APP_WORKDIR/hpans"
-export ACQ_BATCH_INPUT_CRON="0 */2 * * * *"
-export LOG_LEVEL_RTD_TRANSACTION_FILTER="DEBUG"
-export HPAN_SERVICE_URL="https://api.dev.cstar.pagopa.it"
-export ACQ_BATCH_TRX_PAR_ENABLED="false"
-
 mkdir -p $ACQ_BATCH_TOKEN_INPUT_PATH
 mkdir -p $ACQ_BATCH_OUTPUT_PATH
 mkdir -p $ACQ_BATCH_TRX_LOGS_PATH
