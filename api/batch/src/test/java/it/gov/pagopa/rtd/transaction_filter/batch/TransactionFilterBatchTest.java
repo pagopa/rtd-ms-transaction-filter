529d6955e5a436bc4eb3a6f214f79c54c19f6b2d
Alberto Re <alberto.re@pagopa.it>
2022-01-07 09:49:07 +0100

    [CEN-1240] Add some PAR values on test transactions data

--- api/batch/src/test/java/it/gov/pagopa/rtd/transaction_filter/batch/TransactionFilterBatchTest.java
+++ api/batch/src/test/java/it/gov/pagopa/rtd/transaction_filter/batch/TransactionFilterBatchTest.java
@@ -220,13 +220,13 @@ public class TransactionFilterBatchTest {
         Set<String> expectedOutputFileTrnContent = new HashSet<>();
         expectedOutputFileTrnContent.add("13131;00;00;28aa47c8c6cd1a6b0a86ebe18471295796c88269868825b4cd41f94f0a07e88e;03/20/2020 10:50:33;1111111111;5555;;1111;896;22222;0000;1;000002;5422;fis123;12345678901;00;");
         expectedOutputFileTrnContent.add("131331;00;01;e2df0a82ac0aa12921c398e1eba9119772db868650ebef22b8919fa0fb7642ed;03/20/2020 11:23:00;333333333;7777;;3333;896;4444;0000;1;000002;5422;fis123;12345678901;00;");
-        expectedOutputFileTrnContent.add("13131;01;00;805f89015f85948f7d7bdd57a0a81e4cd95fc81bdd1195a69c4ab139f0ebed7b;03/20/2020 11:04:53;2222222222;6666;;2222;896;3333;0000;1;000002;5422;fis123;12345678901;00;");
+        expectedOutputFileTrnContent.add("13131;01;00;805f89015f85948f7d7bdd57a0a81e4cd95fc81bdd1195a69c4ab139f0ebed7b;03/20/2020 11:04:53;2222222222;6666;;2222;896;3333;0000;1;000002;5422;fis123;12345678901;00;par2");
 
         Set<String> expectedOutputFileAdeContent = new HashSet<>();
         expectedOutputFileAdeContent.add("13131;00;00;03/20/2020 10:50:33;1111111111;5555;;1111;896;22222;0000;1;000002;5422;fis123;12345678901;00;");
-        expectedOutputFileAdeContent.add("13131;01;00;03/20/2020 11:04:53;2222222222;6666;;2222;896;3333;0000;1;000002;5422;fis123;12345678901;00;");
+        expectedOutputFileAdeContent.add("13131;01;00;03/20/2020 11:04:53;2222222222;6666;;2222;896;3333;0000;1;000002;5422;fis123;12345678901;00;par2");
         expectedOutputFileAdeContent.add("131331;00;01;03/20/2020 11:23:00;333333333;7777;;3333;896;4444;0000;1;000002;5422;fis123;12345678901;00;");
-        expectedOutputFileAdeContent.add("131331;00;01;03/20/2020 13:23:00;4444444444;8888;;3333;896;4444;0000;1;000002;5422;fis123;12345678901;00;");
+        expectedOutputFileAdeContent.add("131331;00;01;03/20/2020 13:23:00;4444444444;8888;;3333;896;4444;0000;1;000002;5422;fis123;12345678901;00;par4");
 
         Assert.assertEquals(expectedOutputFileTrnContent, new HashSet<>(outputFileTrnContent));
         Assert.assertEquals(expectedOutputFileAdeContent, new HashSet<>(outputFileAdeContent));
@@ -256,7 +256,7 @@ public class TransactionFilterBatchTest {
         File trxFilteredFile = trxFilteredFiles.iterator().next();
         List<String> trxFilteredContent = Files.readAllLines(trxFilteredFile.toPath().toAbsolutePath());
         Assert.assertEquals(2, trxFilteredContent.size());
-        Assert.assertTrue(trxFilteredContent.contains("131331;00;01;pan4;03/20/2020 13:23:00;4444444444;8888;;3333;896;4444;0000;1;000002;5422;fis123;12345678901;00;"));
+        Assert.assertTrue(trxFilteredContent.contains("131331;00;01;pan4;03/20/2020 13:23:00;4444444444;8888;;3333;896;4444;0000;1;000002;5422;fis123;12345678901;00;par4"));
         Assert.assertTrue(trxFilteredContent.contains("131331;00;01;pan5;2020-03-20T13:23:00;555555555;9999;;3333;896;4444;0000;1;000002;5422;fis123;12345678901;00;"));
 
         File trxErrorFile = trxErrorFiles.iterator().next();
