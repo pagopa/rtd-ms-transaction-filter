ffc3feba88f626ccfeefb327a9b6dc61c6901ad4
Alberto Re <alberto.re@pagopa.it>
2022-01-26 11:48:55 +0100

    [CEN-1271] Add retry with exp backoff to REST upload

--- api/batch/src/main/java/it/gov/pagopa/rtd/transaction_filter/batch/step/tasklet/TransactionSenderRestTasklet.java
+++ api/batch/src/main/java/it/gov/pagopa/rtd/transaction_filter/batch/step/tasklet/TransactionSenderRestTasklet.java
@@ -1,23 +1,22 @@
 package it.gov.pagopa.rtd.transaction_filter.batch.step.tasklet;
 
+import feign.FeignException;
 import it.gov.pagopa.rtd.transaction_filter.connector.HpanRestClient;
 import it.gov.pagopa.rtd.transaction_filter.connector.SasResponse;
 import it.gov.pagopa.rtd.transaction_filter.service.HpanConnectorService;
 import lombok.Data;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.core.io.Resource;
-import org.springframework.retry.RetryCallback;
-import org.springframework.retry.backoff.ExponentialBackOffPolicy;
-import org.springframework.retry.policy.SimpleRetryPolicy;
-import org.springframework.retry.support.RetryTemplate;
 import org.springframework.util.Assert;
 
 @Data
+@Slf4j
 public class TransactionSenderRestTasklet implements Tasklet, InitializingBean {
 
     private HpanConnectorService hpanConnectorService;
@@ -29,20 +28,28 @@ public class TransactionSenderRestTasklet implements Tasklet, InitializingBean {
     @SneakyThrows
     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
         if (taskletEnabled) {
-            SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
-            retryPolicy.setMaxAttempts(3);
-            ExponentialBackOffPolicy backoffPolicy = new ExponentialBackOffPolicy();
-            backoffPolicy.setInitialInterval(3000l);
+            int maxAttempts = 3;
+            int remainingAttempts = maxAttempts;
+            int delay = 2;
+            boolean uploadSucceeded = false;
 
-            RetryTemplate createSasTemplate = new RetryTemplate();
-            createSasTemplate.setRetryPolicy(retryPolicy);
-            createSasTemplate.setBackOffPolicy(backoffPolicy);
-            SasResponse sasResponse = createSasTemplate.execute((RetryCallback<SasResponse, Exception>) context -> hpanConnectorService.getSasToken(scope));
-
-            RetryTemplate putFileTemplate = new RetryTemplate();
-            putFileTemplate.setRetryPolicy(retryPolicy);
-            putFileTemplate.setBackOffPolicy(backoffPolicy);
-            putFileTemplate.execute((RetryCallback<Void, Exception>) context -> hpanConnectorService.uploadFile(resource.getFile(), sasResponse.getSas(), sasResponse.getAuthorizedContainer()));
+            SasResponse sasResponse;
+            while (!uploadSucceeded && remainingAttempts > 0) {
+                try {
+                    sasResponse = hpanConnectorService.getSasToken(scope);
+                    hpanConnectorService.uploadFile(resource.getFile(), sasResponse.getSas(), sasResponse.getAuthorizedContainer());
+                    uploadSucceeded = true;
+                } catch (FeignException e) {
+                    remainingAttempts -= 1;
+                    if (remainingAttempts < 1) {
+                        throw e;
+                    }
+                    delay = (int) Math.pow(delay, 2);
+                    log.error(e.getMessage());
+                    log.info("Retrying after " + delay + " seconds (remaining attempts: " + remainingAttempts + ")");
+                    Thread.sleep(delay * 1000l);
+                }
+            }
         }
         return RepeatStatus.FINISHED;
     }
