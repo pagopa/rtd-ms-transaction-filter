32a6674f6c2077a41ebb6738ead7741693cba7ba
Andrea Morabito <andrea.morabito@pagopa.it>
2022-10-20 16:29:24 +0200

    [RTD-671] parametrize script timeout

--- acceptance_tests/different_sender_code_acks/script_multiple_ack.bash
+++ acceptance_tests/different_sender_code_acks/script_multiple_ack.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $# -ne 1 ] ; then
+if [ $# -lt 1 ] ; then
     echo "Illegal number of parameters (1 mandatory, was $#)" >&1
-    echo "usage: bash script_splitting.bash env" >&1
+    echo "usage: bash script_splitting.bash env [timeout_in_minutes]" >&1
     exit 2
 fi
 
@@ -13,6 +13,8 @@ then
 fi
 
 ENV=$1
+# test timeout default 2h (schedule pipeline + execution time pipeline)
+TIMEOUT_IN_MINUTES="${2:-120}"
 
 sh ../common/setup.sh
 
@@ -152,8 +154,6 @@ do
     fi
 done
 
-# test timeout 2h (schedule pipeline + execution time pipeline)
-TIMEOUT_IN_MINUTES=120
 #set batch service send to false in order to not send the placeholder files
 export ACQ_BATCH_TRX_SENDER_ADE_ENABLED=false
 for (( i=0 ; i <= TIMEOUT_IN_MINUTES; i++))
