bdde85ceae827534417ada79e62e1482e6c6650b
Andrea Morabito <andrea.morabito@pagopa.it>
2022-10-24 08:51:20 +0200

    [RTD-674] the script must not support chunking

--- acceptance_tests/ingestion_big_files/script_ingestion.bash
+++ acceptance_tests/ingestion_big_files/script_ingestion.bash
@@ -2,8 +2,9 @@
 
 # This script tests the ingestion of a file of big dimension.
 # By default it will generate a single chunk of 6M rows but it is
-# customizable to generate more chunks by tuning N_AGGREGATES and
-# ACQ_BATCH_WRITER_ADE_SPLIT_THRESHOLD values
+# customizable by tuning N_AGGREGATES value.
+# Keep in mind N_AGGREGATES must not be bigger than the ACQ_BATCH_WRITER_ADE_SPLIT_THRESHOLD.
+# The script does not support the generation of more than one chunk!
 
 if [ $# -ne 1 ] ; then
     echo "Illegal number of parameters (1 mandatory, was $#)" >&1
@@ -46,45 +47,45 @@ java -jar ../common/rtd-ms-transaction-filter.jar
 #### ASSERTIONS
 # 1. check local output files generated
 # set threshold if not present
-if [ -z "$ACQ_BATCH_WRITER_ADE_SPLIT_THRESHOLD" ]
-then
-    ACQ_BATCH_WRITER_ADE_SPLIT_THRESHOLD=1000000
-fi
+#if [ -z "$ACQ_BATCH_WRITER_ADE_SPLIT_THRESHOLD" ]
+#then
+#    ACQ_BATCH_WRITER_ADE_SPLIT_THRESHOLD=1000000
+#fi
 
 # find chunks number
-if [ $N_AGGREGATES -lt $ACQ_BATCH_WRITER_ADE_SPLIT_THRESHOLD ]
-then
-	  N_CHUNKS=1
-else
-	  N_CHUNKS=$(( ( N_AGGREGATES / ACQ_BATCH_WRITER_ADE_SPLIT_THRESHOLD ) + ( N_AGGREGATES % ACQ_BATCH_WRITER_ADE_SPLIT_THRESHOLD > 0 ) ))
-fi
+#if [ $N_AGGREGATES -lt $ACQ_BATCH_WRITER_ADE_SPLIT_THRESHOLD ]
+#then
+#	  N_CHUNKS=1
+#else
+#	  N_CHUNKS=$(( ( N_AGGREGATES / ACQ_BATCH_WRITER_ADE_SPLIT_THRESHOLD ) + ( N_AGGREGATES % ACQ_BATCH_WRITER_ADE_SPLIT_THRESHOLD > 0 ) ))
+#fi
 
 OUTPUT_WITHOUT_EXTENSION="ADE.$(echo "$FILENAME" | cut -d'.' -f2-6)"
 # check output files generated
-for (( i=1; i<=N_CHUNKS; i++ ))
-do
-    # there is a limitation to 9 chunks!!!!
-    FILENAME_CHUNKED="$OUTPUT_WITHOUT_EXTENSION.0$i.csv"
-    if test -f "$ACQ_BATCH_OUTPUT_PATH/$FILENAME_CHUNKED"; then
-        echo "$FILENAME_CHUNKED exists: [SUCCESS]"
-    else
-        echo "$FILENAME_CHUNKED does not exist: [FAILED]"
-        exit 2
-    fi
-done
+#for (( i=1; i<=N_CHUNKS; i++ ))
+#do
+
+FILENAME_CHUNKED="$OUTPUT_WITHOUT_EXTENSION.01.csv"
+if test -f "$ACQ_BATCH_OUTPUT_PATH/$FILENAME_CHUNKED"; then
+    echo "$FILENAME_CHUNKED exists: [SUCCESS]"
+else
+    echo "$FILENAME_CHUNKED does not exist: [FAILED]"
+    exit 2
+fi
+#done
 
 # 2. check application log to find evidence of successful files upload
 N_UPLOADS=$(grep -c "uploaded with success (status was: 201)" < workdir/logs/application.log)
-if [ $N_CHUNKS -ne "$N_UPLOADS" ]
+if [ "$N_UPLOADS" -ne 1 ]
 then
-    echo "Upload test not passed, $N_CHUNKS chunks vs $N_UPLOADS uploaded: [FAILED]"
+    echo "Upload test not passed, $N_UPLOADS files uploaded: [FAILED]"
     exit 2
 else
-	  echo "Files uploaded with success: [SUCCESS]"
+	  echo "File uploaded with success: [SUCCESS]"
 fi
 
 # cli atm do not support chunk generation, it can be arranged like this:
-LOCAL_OUTPUT_DIFF=$(diff <(cat cstar-cli/generated/ADE.*expected | sort) <(cat workdir/output/ADE.*.csv | sort | tail -n +$N_CHUNKS))
+LOCAL_OUTPUT_DIFF=$(diff <(cat cstar-cli/generated/ADE.*expected | sort) <(cat workdir/output/ADE.*.csv | sort | tail -n +1))
 
 if [ -z "$LOCAL_OUTPUT_DIFF" ]
 then
