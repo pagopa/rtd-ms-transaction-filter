f06bdce107a751bcc322daf428a14bfed4e27e20
Andrea Morabito <andrea.morabito@pagopa.it>
2022-10-31 18:30:37 +0100

    [RTD-1045] rebasing

--- integration/rest/src/test/java/it/gov/pagopa/rtd/transaction_filter/connector/FileReportRestClientTest.java
+++ integration/rest/src/test/java/it/gov/pagopa/rtd/transaction_filter/connector/FileReportRestClientTest.java
@@ -39,7 +39,9 @@ import org.springframework.test.context.support.TestPropertySourceUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@TestPropertySource(locations = "classpath:config/rest-client.properties", properties = {
+@TestPropertySource(
+    locations = "classpath:config/rest-client.properties",
+    properties = {
     "rest-client.hpan.list.url=/rtd/payment-instrument-manager/hashed-pans",
     "rest-client.hpan.salt.url=/rtd/payment-instrument-manager/salt",
     "rest-client.hpan.adesas.url=/rtd/csv-transaction/ade/sas",
@@ -65,7 +67,10 @@ public class FileReportRestClientTest {
 
   @ClassRule
   public static WireMockClassRule wireMockRule = new WireMockClassRule(
-      wireMockConfig().dynamicHttpsPort().dynamicPort().needClientAuth(true)
+      wireMockConfig()
+          .dynamicHttpsPort()
+          .dynamicPort()
+          .needClientAuth(true)
           .keystorePath("src/test/resources/certs/server-keystore.jks").keystorePassword("secret")
           .keyManagerPassword("secret")
           .trustStorePath("src/test/resources/certs/server-truststore.jks")
