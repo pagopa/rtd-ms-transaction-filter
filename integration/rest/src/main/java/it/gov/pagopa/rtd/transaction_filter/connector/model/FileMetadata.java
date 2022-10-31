6998b0ac26d07ae847412f8a381e2bbc7202c9d3
Andrea Morabito <andrea.morabito@pagopa.it>
2022-10-31 18:28:28 +0100

    [RTD-839] add validators

--- integration/rest/src/main/java/it/gov/pagopa/rtd/transaction_filter/connector/model/FileMetadata.java
+++ integration/rest/src/main/java/it/gov/pagopa/rtd/transaction_filter/connector/model/FileMetadata.java
@@ -1,14 +1,28 @@
 package it.gov.pagopa.rtd.transaction_filter.connector.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.time.LocalDateTime;
-import lombok.Builder;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
-@Builder
 public class FileMetadata {
 
-  String fileName;
-  long size;
-  LocalDateTime transmissionDate;
+  @NotNull
+  @NotBlank
+  private String fileName;
+
+  @NotNull
+  private long size;
+
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+  @JsonSerialize(using = LocalDateTimeSerializer.class)
+  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+  @NotNull
+  private LocalDateTime transmissionDate;
 }
--- integration/rest/src/main/java/it/gov/pagopa/rtd/transaction_filter/connector/model/FileReport.java
+++ integration/rest/src/main/java/it/gov/pagopa/rtd/transaction_filter/connector/model/FileReport.java
@@ -1,11 +1,9 @@
 package it.gov.pagopa.rtd.transaction_filter.connector.model;
 
 import java.util.Collection;
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class FileReport {
 
   Collection<FileMetadata> files;
