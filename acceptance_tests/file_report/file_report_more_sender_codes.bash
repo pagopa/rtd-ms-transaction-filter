11ff00538bf759d0835ea0041b6a57c749ee44b6
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-12-09 15:26:38 +0100

    [RTD-1121] refactor and add comments

--- acceptance_tests/file_report/file_report_more_sender_codes.bash
+++ acceptance_tests/file_report/file_report_more_sender_codes.bash
@@ -17,14 +17,16 @@ ENV=$1
 # timeout default 5minutes
 TIMEOUT_IN_MINUTES="${2:-5}"
 
+# Set sender codes
 SENDER_CODE1="${3-12345}"
 SENDER_CODE2="${4-54321}"
 
-sh ../common/setup.sh
 
 N_AGGREGATES=8
 
 REPORT_DIR=workdir/reports
+
+sh ../common/setup.sh
 
 echo "Make sure to have $SENDER_CODE1 and $SENDER_CODE2 associated with your API key"
 echo "Make sure to have empty cstar-cli/generated folder"
@@ -49,7 +51,7 @@ get_file_sent_occurrence_in_report() {
     FILES_SENT_OCCURRENCES_IN_REPORT=$(grep -c "$OUTPUT_FILENAME" < "$REPORT_DIR"/"$FILE_REPORT_NAME")
 }
 check_two_files_sent() {
-  # check if file has been uploaded
+  # check if the files has been uploaded
   N_UPLOADS=$(grep -c "uploaded with success (status was: 201)" < workdir/logs/application.log)
   if [ "$N_UPLOADS" -ne 2 ]
   then
@@ -68,21 +70,26 @@ generate_input_file
 source ../common/setenv_"$ENV".sh
 source setenv.sh
 
+# Disable batch scheduling for processing both files independently
 export ACQ_BATCH_SCHEDULED=false
+
+# Explicitly set the report directory
 export ACQ_BATCH_FILE_REPORT_PATH="$REPORT_DIR"
 
+# Batch service run for both files
 echo "Executing batch service..."
-### batch service run
 run_batch_service
 run_batch_service
-#### ASSERTIONS
 
+#### ASSERTIONS
 check_two_file_sent
 
 SLEEP_INTERVAL_IN_SECONDS=10
 
-#set batch service send to false in order to not send the placeholder files
+# Set batch service send to false in order to not send the placeholder files
 export ACQ_BATCH_TRX_SENDER_ADE_ENABLED=false
+
+# Wait for the report to be returned containing the two files
 for (( i=0 ; i <= TIMEOUT_IN_MINUTES * 60 / SLEEP_INTERVAL_IN_SECONDS; i++))
 do
     echo "Waiting $SLEEP_INTERVAL_IN_SECONDS seconds..."
@@ -95,7 +102,7 @@ do
     # check if file sent in the previous run has been received inside the report
     get_file_sent_occurrence_in_report
 
-    # if report does contain the file sent the exit loop
+    # if report does contain the files sent the exit loop
     if [ "$FILES_SENT_OCCURRENCES_IN_REPORT" -gt 0 ]
     then
         echo "Files found in report: [SUCCESS]"
