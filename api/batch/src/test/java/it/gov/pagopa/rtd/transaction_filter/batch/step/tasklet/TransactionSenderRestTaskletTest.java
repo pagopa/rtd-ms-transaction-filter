1acb0174cf788ab280ef99c21458618832a0499e
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2023-02-14 12:15:34 +0100

    fix: [RTD-1356] fix retry on sas failure (#341)

--- api/batch/src/test/java/it/gov/pagopa/rtd/transaction_filter/batch/step/tasklet/TransactionSenderRestTaskletTest.java
+++ api/batch/src/test/java/it/gov/pagopa/rtd/transaction_filter/batch/step/tasklet/TransactionSenderRestTaskletTest.java
@@ -2,12 +2,16 @@ package it.gov.pagopa.rtd.transaction_filter.batch.step.tasklet;
 
 import static org.mockito.ArgumentMatchers.any;
 
+import feign.FeignException;
+import feign.Request;
+import feign.RequestTemplate;
 import it.gov.pagopa.rtd.transaction_filter.connector.HpanRestClient;
 import it.gov.pagopa.rtd.transaction_filter.connector.SasResponse;
 import it.gov.pagopa.rtd.transaction_filter.service.HpanConnectorService;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
@@ -112,6 +116,32 @@ class TransactionSenderRestTaskletTest {
         Assert.assertThrows(IOException.class, () -> transactionSenderRestTasklet.execute(new StepContribution(execution), chunkContext));
 
         BDDMockito.verify(hpanConnectorServiceMock, Mockito.times(3)).uploadFile(any(), any(), any());
+    }
+
+    @SneakyThrows
+    @Test
+    void whenSasTokenFailsThenRetry() {
+        Path fileToSend = Files.createFile(tempFolder.resolve("test"));
+        Request request = Request.create(Request.HttpMethod.PUT, "url",
+            new HashMap<>(), null, new RequestTemplate());
+        BDDMockito.given(hpanConnectorServiceMock.getSasToken(any()))
+            .willAnswer(invocation -> { throw new FeignException.InternalServerError("", request, null, new HashMap<>()); });
+
+        TransactionSenderRestTasklet transactionSenderRestTasklet = new TransactionSenderRestTasklet();
+        transactionSenderRestTasklet.setTaskletEnabled(true);
+        transactionSenderRestTasklet.setHpanConnectorService(this.hpanConnectorServiceMock);
+        transactionSenderRestTasklet.setResource(new UrlResource("file:/" + fileToSend));
+        transactionSenderRestTasklet.setScope(HpanRestClient.SasScope.ADE);
+        transactionSenderRestTasklet.setInitialDelayInSeconds(0);
+
+        StepExecution execution = MetaDataInstanceFactory.createStepExecution();
+        StepContext stepContext = new StepContext(execution);
+        ChunkContext chunkContext = new ChunkContext(stepContext);
+        StepContribution stepContribution = new StepContribution(execution);
+        // after three retries the exception will be thrown out anyway
+        Assert.assertThrows(FeignException.class, () -> transactionSenderRestTasklet.execute(stepContribution, chunkContext));
+
+        BDDMockito.verify(hpanConnectorServiceMock, Mockito.times(3)).getSasToken(any());
     }
 
     @SneakyThrows
