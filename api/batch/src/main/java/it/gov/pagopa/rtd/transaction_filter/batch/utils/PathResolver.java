2e4732f8ed1ed561eeb08232d76acf9439d469fc
Andrea Morabito <andrea.morabito@pagopa.it>
2024-08-21 17:04:45 +0200

    improve readability

--- api/batch/src/main/java/it/gov/pagopa/rtd/transaction_filter/batch/utils/PathResolver.java
+++ api/batch/src/main/java/it/gov/pagopa/rtd/transaction_filter/batch/utils/PathResolver.java
@@ -10,18 +10,19 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 public class PathResolver {
 
   public static final String FILE_PREFIX = "file:";
+  public static final String REGEX_CSV_EXTENSION = "/*.csv";
   private final PathMatchingResourcePatternResolver resolver;
 
   /**
-   * Resolve symlinks for <code>file:</code> path and retrieve all the Resources inside the target path with csv format.
-   * Other path type e.g. <code>classpath:</code> won't resolve symlink.
+   * Resolve symlinks for <code>file:</code> path and retrieve all the Resources inside the target
+   * path with csv format. Other path type e.g. <code>classpath:</code> won't resolve symlink.
    *
    * @return array of Resource
    * @throws IOException if the file does not exist
    */
   public Resource[] getCsvResources(String path) throws IOException {
     var targetPathWithPrefix = resolveSymlink(path);
-    return resolver.getResources(targetPathWithPrefix + "/*.csv");
+    return resolver.getResources(targetPathWithPrefix + REGEX_CSV_EXTENSION);
   }
 
   private String resolveSymlink(String symlink) throws IOException {
