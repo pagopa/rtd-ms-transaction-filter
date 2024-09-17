cdf805a80e6b0ab6a573d29a0a35c5a9d540421f
Andrea Morabito <andrea.morabito@pagopa.it>
2024-09-17 13:06:58 +0200

    update test class to junit 5

--- api/batch/src/test/java/it/gov/pagopa/rtd/transaction_filter/batch/step/tasklet/SelectTargetInputFileTaskletTest.java
+++ api/batch/src/test/java/it/gov/pagopa/rtd/transaction_filter/batch/step/tasklet/SelectTargetInputFileTaskletTest.java
@@ -1,21 +1,22 @@
 package it.gov.pagopa.rtd.transaction_filter.batch.step.tasklet;
 
-import static org.mockito.Mockito.reset;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import it.gov.pagopa.rtd.transaction_filter.service.StoreService;
 import java.io.IOException;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.rules.TemporaryFolder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
 import org.mockito.BDDMockito;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.StepExecution;
@@ -23,7 +24,7 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.scope.context.StepContext;
 import org.springframework.batch.test.MetaDataInstanceFactory;
 
-
+@ExtendWith(MockitoExtension.class)
 public class SelectTargetInputFileTaskletTest {
 
     private ChunkContext chunkContext;
@@ -32,68 +33,58 @@ public class SelectTargetInputFileTaskletTest {
     @Mock
     private StoreService storeServiceMock;
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+    @TempDir
+    private Path tempFolder;
 
-    @Rule
-    public TemporaryFolder tempFolder = new TemporaryFolder();
-
-    public SelectTargetInputFileTaskletTest(){
-        MockitoAnnotations.initMocks(this);
-    }
-
-    @BeforeClass
+    @BeforeAll
     public static void configTest() {
         Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
         root.setLevel(Level.INFO);
     }
 
-    @Before
-    public void setUp() throws IOException {
-        reset(storeServiceMock);
-
+    @BeforeEach
+    public void setUp() {
         execution = MetaDataInstanceFactory.createStepExecution();
         StepContext stepContext = new StepContext(execution);
         chunkContext = new ChunkContext(stepContext);
     }
 
     @Test
-    public void shouldQuitJobWhenNoInputFilesAreFound() throws IOException {
+    void shouldQuitJobWhenNoInputFilesAreFound() {
         SelectTargetInputFileTasklet tasklet = new SelectTargetInputFileTasklet();
         tasklet.setStoreService(storeServiceMock);
-        tasklet.setTransactionDirectoryPath("file:" + tempFolder.getRoot().getAbsolutePath());
+        tasklet.setTransactionDirectoryPath("file:" + tempFolder.toAbsolutePath());
 
-        expectedException.expect(IOException.class);
-        tasklet.execute(new StepContribution(execution), chunkContext);
+        assertThrows(IOException.class, () -> tasklet.execute(new StepContribution(execution), chunkContext));
         BDDMockito.verify(storeServiceMock, Mockito.times(0)).setTargetInputFile(Mockito.any());
     }
 
     @Test
-    public void shouldSelectTheOnlyOneFilePresent() throws IOException {
+    void shouldSelectTheOnlyOneFilePresent() throws IOException {
         String inputFilename = "CSTAR.99999.TRNLOG.20220419.065813.001.csv";
-        tempFolder.newFile(inputFilename);
+        Files.createFile(tempFolder.resolve(inputFilename));
 
         SelectTargetInputFileTasklet tasklet = new SelectTargetInputFileTasklet();
         tasklet.setStoreService(storeServiceMock);
-        tasklet.setTransactionDirectoryPath("file:" + tempFolder.getRoot().getAbsolutePath());
+        tasklet.setTransactionDirectoryPath("file:" + tempFolder.toAbsolutePath());
 
         tasklet.execute(new StepContribution(execution), chunkContext);
-        BDDMockito.verify(storeServiceMock, Mockito.times(1)).setTargetInputFile(inputFilename);
+        BDDMockito.verify(storeServiceMock).setTargetInputFile(inputFilename);
     }
 
     @Test
-    public void shouldSelectTheOlderFilePresent() throws IOException {
-        tempFolder.newFile("CSTAR.99999.TRNLOG.20220418.065813.001.csv");
-        tempFolder.newFile("CSTAR.99999.TRNLOG.20220418.065813.002.csv");
-        tempFolder.newFile("CSTAR.99999.TRNLOG.20220418.122117.001.csv");
-        tempFolder.newFile("CSTAR.99999.TRNLOG.20220419.065813.001.csv");
+    void shouldSelectTheOlderFilePresent() throws IOException {
+        Files.createFile(tempFolder.resolve("CSTAR.99999.TRNLOG.20220418.065813.001.csv"));
+        Files.createFile(tempFolder.resolve("CSTAR.99999.TRNLOG.20220418.065813.002.csv"));
+        Files.createFile(tempFolder.resolve("CSTAR.99999.TRNLOG.20220418.122117.001.csv"));
+        Files.createFile(tempFolder.resolve("CSTAR.99999.TRNLOG.20220419.065813.001.csv"));
 
         SelectTargetInputFileTasklet tasklet = new SelectTargetInputFileTasklet();
         tasklet.setStoreService(storeServiceMock);
-        tasklet.setTransactionDirectoryPath("file:" + tempFolder.getRoot().getAbsolutePath());
+        tasklet.setTransactionDirectoryPath("file:" + tempFolder.toAbsolutePath());
 
         tasklet.execute(new StepContribution(execution), chunkContext);
-        BDDMockito.verify(storeServiceMock, Mockito.times(1)).setTargetInputFile("CSTAR.99999.TRNLOG.20220418.065813.001.csv");
+        BDDMockito.verify(storeServiceMock).setTargetInputFile("CSTAR.99999.TRNLOG.20220418.065813.001.csv");
     }
 
 }
